4f822bd7a325ea095cfd2ae1bceb1f9a87421f77
0502j <jamiledsousa@gmail.com>
2023-01-16 14:47:23 -0300

    Adding routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <></>
+    <Routes>
+        <Route path="/" element={<Register/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+    </Routes>
   );
 }
 
