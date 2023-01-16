40e566ce9eba02b19a0337f55c8eb8c01d978e95
0502j <jamiledsousa@gmail.com>
2023-01-16 15:19:01 -0300

    Structuring register page

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,9 +1,16 @@
 import classes from './pages-css/Register.module.css';
+import img from '../img/background.png';
 
 const Register = () => {
     return(
-        <div>
-            <h1>Register page</h1>
+        <div className={classes.registermain}>
+            <div className={classes.introtext}>
+                <h1>Welcome,</h1>
+                <h2>Please, register to continue</h2>
+            </div>
+            <div className={classes.registerimg}>
+                <img src={img}/>
+            </div>
         </div>
     );
 };
