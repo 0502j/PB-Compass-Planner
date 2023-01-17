93800cb025fc9e31b2f9a6efe0653d9438bed5ba
0502j <jamiledsousa@gmail.com>
2023-01-17 14:16:07 -0300

    Fixing Input component props

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,7 +2,7 @@
 const Input = (props) => {
     return(
         <div>
-            <input className={props.className} type={props.type} id={props.id} placeholder={props.placeholder}/>
+            <input onChange={props.onChange} className={props.className} type={props.type} id={props.id} placeholder={props.placeholder}/>
         </div>
     );
 }
