0f54c01cc04ed236990a8222db55c1466b269fbb
0502j <jamiledsousa@gmail.com>
2023-01-17 14:12:52 -0300

    Separating background into a component

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -3,22 +3,22 @@ import img from '../img/background.png';
 import {Fragment} from 'react';
 import RegisterForm from '../components/RegisterForm';
 import { Link } from 'react-router-dom';
+import IntroSection from '../components/IntroSection';
 
 const Register = () => {
     return(
+
         <Fragment>
-            <div className={classes.registermain}>
-                <div className={classes.introtext}>
-                    <h1>Welcome,</h1>
-                    <h2>Please, register to continue</h2>
-                    <RegisterForm/>
-                    <Link to='/login'>Already have an account? Sign in.</Link>
-                </div>
-                <div className={classes.registerimg}>
-                    <img src={img}/>
-                </div>
-            </div>
+            
+            <IntroSection>
+                <h1>Welcome,</h1>
+                <h2>Please, register to continue</h2>
+                <RegisterForm/>
+                <Link to='/login'>Already have an account? Sign in.</Link>
+            </IntroSection>
+
         </Fragment>
+
     );
 };
 
---
+++ src/components/IntroSection.js
@@ -0,0 +1,19 @@
+import classes from '../pages/pages-css/Register.module.css';
+import img from '../img/background.png';
+
+const IntroSection = (props) => {
+    
+    return(
+        <div className={classes.registermain}>
+            <div className={classes.introtext}>
+                {props.children}
+            </div>
+            <div className={classes.registerimg}>
+                <img src={img}/>
+            </div>
+        </div>
+    );
+
+};
+
+export default IntroSection;
