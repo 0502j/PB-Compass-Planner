f4adb8b46bea5163115293ce702d6d9fc4156d1f
0502j <jamiledsousa@gmail.com>
2023-01-18 19:54:21 -0300

    Adding date & time on header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,51 @@
+import { useEffect, useState } from 'react';
+import classes from '../css-components/Header.module.css';
+
 const Header = () => {
+
+    //Fetch month name instead of number
+    Date.prototype.getMonthName = function(){
+        var monthNames = ["January", "February","March","April","May",
+    "June","July","August", "September", "October", "November", "December"];
+
+        return monthNames[this.getMonth()];
+    }
+
+
+   //Update time every 1 minute
+   const [time, setTime] = useState(new Date().toLocaleTimeString().slice(0,-3));
+   let month = new Date().getMonthName();
+   let day = new Date().getDate();
+   let year = new Date().getFullYear();
+   
+   let fulldate = month + " " + day + ", " + year;
+  
+
+    useEffect(()=>{
+        const timer = setInterval(()=>{
+            setTime(new Date().toLocaleTimeString().slice(0,-3));
+            
+        },60*1000);
+        return () => {
+            clearInterval(timer);
+        }
+    },[]);
+
+    
     return(
-        <div>Dashboard header</div>
+        <div className={classes.mainct}>
+            <div className={classes.headertitle}>
+                <h2>Weekly Planner</h2>
+                <p>Use this planner to organize your daily issues.</p>
+            </div>
+
+            <div className={classes.datetime}>
+                    <h1>{time}</h1>
+                    <p>{fulldate}</p>
+                   
+            </div>
+        </div>
     );
 }
+
+export default Header;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,12 +1,14 @@
 import classes from './pages-css/Dashboard.module.css';
 import { AuthContext } from '../store/user-context';
 import { useState, useEffect, useContext } from 'react';
+import Header from '../components/Header';
 
 const Dashboard = () => {
 
     //render different content based on useContext info
     const {isLogged, setIsLogged} = useContext(AuthContext);
-    const content = isLogged ? <h1>Welcome to the dashboard!</h1> : <h1>Not allowed!</h1>
+    const loggedcontent = (<div><Header></Header></div>);
+    const content = isLogged ? loggedcontent : <h1>Not allowed!</h1>
 
     return(
         <div>
