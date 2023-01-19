0c855541f344bc8c0015e69b73bc484320fff09d
0502j <jamiledsousa@gmail.com>
2023-01-18 21:17:55 -0300

    Adding logo & logout on header, structuring basic weather API logic

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import classes from '../css-components/Header.module.css';
+import compassLogo from '../img/compassLogo.svg';
+import arrow from '../img/arrow.svg';
 
 const Header = () => {
 
@@ -31,6 +33,22 @@ const Header = () => {
         }
     },[]);
 
+    //Weather API fetching
+    const [weather, setWeather] = useState();
+    const KEY = process.env.REACT_APP_API_KEY;
+   
+
+    const weatherAPIData = async () => {     
+        const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=Pirassununga&appid=KEY")
+        .then((response)=>response.json());
+
+        setWeather(response);
+    }
+
+    useEffect(()=>{
+        weatherAPIData();
+        console.log(weather);
+    },[]);
     
     return(
         <div className={classes.mainct}>
@@ -42,7 +60,18 @@ const Header = () => {
             <div className={classes.datetime}>
                     <h1>{time}</h1>
                     <p>{fulldate}</p>
-                   
+            </div>
+
+            <div className={classes.weatherdiv}>
+                <h3>Dummy weather text</h3>
+            </div>
+
+            <div className={classes.logoutdiv}>
+                <div><img src={compassLogo}/></div>
+                <div className={classes.logoutcontent}>
+                    <img src={arrow}/>
+                    <h3>Logout</h3>
+                </div>
             </div>
         </div>
     );
