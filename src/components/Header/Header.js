f932a69ef1d0684f94757f2df6b79da21df10821
0502j <jamiledsousa@gmail.com>
2023-01-29 09:33:58 -0300

    Fixing day suffix logic

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,19 +9,20 @@ import TemperatureLogo from '../../img/TemperatureLogo.svg';
 
 const Header = () => {
 
-    
     //Adding suffix on day number
     const DaySuffix = (day) => {
-        if(day>3 && day<21){
-            return 'th';
+        if (day > 3 && day < 21) return "th";
+        switch (day % 10) {
+          case 1:
+            return "st";
+          case 2:
+            return "nd";
+          case 3:
+            return "rd";
+          default:
+            return "th";
         }
-        switch(day%10){
-            case 1: return "st";
-            case 2: return "nd";
-            case 3: return "rd";
-            case 4: return "th";
-        }
-    }
+      };
 
     //Fetch month name instead of number
     Date.prototype.getMonthName = function(){
